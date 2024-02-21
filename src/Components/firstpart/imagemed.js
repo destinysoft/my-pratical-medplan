0b19965cc46b802513c1de3abe670a9582e03c1b
Your Name <you@example.com>
2024-02-21 16:46:04 +0100

    another commit

--- src/Components/firstpart/imagemed.js
+++ src/Components/firstpart/imagemed.js
@@ -23,20 +23,18 @@ const Imagemed = ( { isOpen }) => {
     <div  className='timemed'>
      <img className="timeimg" src={Images[currentIndex]} alt={`image ${currentIndex + 1}`} />
 
-     <div className='bbb'>
-        <img className='backimg'  src={image1} alt='img'  />
-
         <div className='timecaption'>  
+        <div className='bbb'>
       {
-         currentIndex === 0 ?  <div> <span className='a'>MEDPLAN SOLUTIONS</span> <br/>  <div className='b'> Improving Healthcare In <span className='c'>Africa</span><br/>Using <span  className='d'>Digital Health</span> Solution </div> </div>
+         currentIndex === 0 ?  <div> <span className='a'>MEDPLAN SOLUTIONS</span> <br/>  <br/> <div className='b'> Improving Healthcare In <span className='c'>Africa</span><br/>Using <span  className='d'>Digital Health</span> Solution </div> </div>
          : currentIndex === 1 ? <div> <span  className='e'>From</span>  <span className='f'>Telemedicine</span> <span className='g'>to</span> <div className='h'> Personalized Care <span className='i'>Apps,</span><br/> <span className='j'> Explore the Ways of our Solutions <br/>are Revolutionizing Healthcare <br/> in Africa</span> </div></div>
          :  <div> <span className='k'>Delivering</span> <span className='l'>Patient centered</span> <div className='m'> Care <span className='n'>at pivotal moment in their <br/></span> <span  className='o'>treatment journey</span> </div> </div>
   
         }
-      
+       </div>
      </div> 
+   
      </div>
-  </div>
   
   )
   
